5d97b0be46710655ae48e2f3fca1d5d9051f4e33
kelvin3127 <kelvin3127@users.noreply.github.com>
2020-10-29 17:24:36 -0400

    Added Error Handling

--- src/Components/Pages/PokeHome.js
+++ src/Components/Pages/PokeHome.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import "./PokeHome.css"
+
 import PokeNav from '../Layout/PokeNav';
 import PokeSearch from '../Layout/PokeSearch';
 import PokeCard from '../Layout/PokeCard';
 import { fetchPokemon } from '../Services/getPokemon';
+import { Spinner, Alert } from 'react-bootstrap';
 
 
 const PokeHome =  () => {
@@ -16,19 +19,42 @@ const PokeHome =  () => {
     //Only true when a call is made ie when button is pressed for pokemon
     const [ loading, setLoading] = React.useState(false);
 
+    //State variable for Error handling if error occurs
+    //Only true when a call is made that leads to a error
+    const [ error, setError] = React.useState(false);
+    const [ errorMsg, setErrorMsg] = React.useState('');
+
 
     //Function to take paramater from child component (PokeSearch) when called
     //Query is the data or pokemon name that we are grabbing from the child component
     //Make  pokeApi call here
     const getPokemon = async (query) => {
+        //Error Handling if empty search
+        if (!query) {
+            setErrorMsg("You must enter a Pokemon");
+            setError(true);
+            return;
+        }
+        setError(false);
         setLoading(true);
-        //Returns us a response which is the function fetch pokemon where we grab our url
-        const response = await fetchPokemon(query);
-        //Call response.json to get the data we want which is json data
-        const results = await response.json();
-        //Call setPokemon and set the results into pokemon
-        setPokemon(results);
-        setLoading(false);
+        //Set timeout to add spinner for loading
+        setTimeout( async () => {
+            //try and catch for error handling for wrong pokemon name
+            try {
+                //Returns us a response which is the function fetch pokemon where we grab our url
+                const response = await fetchPokemon(query);
+                //Call response.json to get the data we want which is json data
+                const results = await response.json();
+                //Call setPokemon and set the results into pokemon
+                setPokemon(results);
+                setLoading(false);
+            } catch (err) {
+                console.log(err);
+                setLoading(false);
+                setError(true);
+                setErrorMsg('Pokemon not Found.');
+            }
+        }, 1500);
 
     }
     
@@ -38,7 +64,17 @@ const PokeHome =  () => {
         <React.Fragment>
 
             <PokeNav />
+            {error ? (
+                <Alert variant="danger">
+                    {errorMsg}
+                </Alert>
+            ) : null }
             <PokeSearch getPokemon={getPokemon}/>
+            {loading ? (
+                <div className="PokeSpinner">
+                    <Spinner animation="border"/>
+                </div>
+                 ) : null }
             {!loading && pokemon ? (
                     <PokeCard 
                         name={pokemon.name}
