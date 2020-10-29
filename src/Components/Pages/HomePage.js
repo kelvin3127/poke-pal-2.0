86d6618756483ff621ffc8d0bdb5824fd5fde9dc
kelvin3127 <kelvin3127@users.noreply.github.com>
2020-10-29 14:53:33 -0400

    Addded Navbar

---
+++ src/Components/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PokeNav from '../Layout/PokeNav';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <PokeNav />
+            </div>
+        )
+    }
+}
